650d4a81f7e7ae7e0f44eb8410da89da2b46130e
Floryan MOLLET <floryan@kanope.io>
2024-08-13 21:53:15 +0200

    Feat: Use the dataFile given with the test. Refactor the test file and the function to match the object type

--- filter.js
+++ filter.js
@@ -1,18 +1,23 @@
 function filter(data, pattern) {
   if (!data || data.length === 0) {
-    return 'Invalid data: One or more countries are empty or improperly formatted.'
+    return 'Invalid data: One or more countries are empty or improperly formatted.';
   }
-  return data
-    .map(country => (
-      {
+  console.log(data.data);
+  const filteredData = data.data
+    .map(country => ({
       name: country.name,
-      peoples: country.peoples.map(people => ({
-        name: people.name,
-        animals: people.animals.filter(animal =>
-          animal.toLowerCase().includes(pattern.toLowerCase())
+      peoples: country.people.map(p => ({
+        name: p.name,
+        animals: p.animals.filter(animal =>
+          animal.name.toLowerCase().includes(pattern.toLowerCase())
         )
       })).filter(people => people.animals.length > 0)
-    })).filter(country => country.peoples.length > 0);
+    }))
+    .filter(country => country.peoples.length > 0);
+  if (filteredData.length === 0) {
+    return 'No matching data found for the given pattern'
+  }
+  return filteredData;
 }
 
 module.exports = filter;
--- test/filter.test.js
+++ test/filter.test.js
@@ -1,69 +1,78 @@
 const filter = require('../filter');
+const dataFile = require('../data');
 
-const data = [
-  {
-    name: "Australia",
-    peoples: [
-      {
-        name: "Aboriginals",
-        animals: ["Kangaroo", "Koala", "Emu", "Rat"]
-      }
-    ]
-  },
-  {
-    name: "Africa",
-    peoples: [
-      {
-        name: "Masai",
-        animals: ["Lion", "Zebra", "Buffalo"]
-      }
-    ]
-  },
-  {
-    name: "France",
-    peoples: [
-      {
-        name: "Pierre",
-        animals: [],
-      }
-    ]
-  },
-  {
-    name: "Italia",
-    peoples: [],
-  },
-];
-
-test('Filter animals with pattern "ra"', () => {
-  const pattern = 'ra';
-  const result = filter(data, pattern);
+test('Filter animals with pattern "as"', () => {
+  const pattern = 'as';
+  const result = filter(dataFile, pattern);
 
   expect(result).toEqual([
     {
-      name: "Australia",
+      name: "Tohabdal",
       peoples: [
         {
-          name: "Aboriginals",
-          animals: ["Rat"]
+          name: "Effie Houghton",
+          animals: [
+            {name: 'African Wild Ass'}
+          ]
+        },
+        {
+          name: 'Owen Bongini',
+          animals:
+            [{name: 'Zebrashark'}]
         }
       ]
     },
     {
-      name: "Africa",
+      name: "Uzuzozne",
       peoples: [
         {
-          name: "Masai",
-          animals: ["Zebra"]
-        }
+          name: 'Georgia Hooper',
+          animals: [
+            {name: 'Grasshopper'}
+          ]
+        },
       ]
-    }
+    },
+    {
+      name: "Zuhackog",
+      peoples: [
+        {
+          name: 'Elva Baroni',
+          animals: [
+            {name: 'Zebrashark'},
+          ]
+        },
+      ]
+    },
+    {
+      name: "Satanwi",
+      peoples: [
+        {
+          name: 'Elmer Kinoshita',
+          animals: [
+            {name: 'Weasel'}]
+        },
+        {
+          name: 'Ernest Conte',
+          animals:
+            [{name: 'Asian Elephant'}]
+        },
+      ]
+    },
   ]);
 });
 
-test('error message validation', () => {
+test('error message if no data', () => {
   const pattern = 'xyz';
   const dataTest = [];
   const result = filter(dataTest, pattern);
 
   expect(result).toEqual('Invalid data: One or more countries are empty or improperly formatted.');
+});
+
+test('error message if pattern return no result', () => {
+  const pattern = 'xyz';
+  const result = filter(dataFile, pattern);
+
+  expect(result).toEqual('No matching data found for the given pattern');
 });
