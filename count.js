ad97d2e7ef16932533d17553249e23992484dea5
Floryan MOLLET <floryan@kanope.io>
2024-08-16 15:45:04 +0200

    Fix: Add two conditions, check if there is data in people and animals before continue

--- count.js
+++ count.js
@@ -3,10 +3,16 @@ function count(data) {
     return 'Invalid data: please provide a valid data file';
   }
   const dataCounter = data.data.map(country => {
+    if (country.people.length === 0) {
+      return ('No people in the country');
+    }
     const countPeoples = country.people.length;
     return {
       name: `${country.name} [${countPeoples}]`,
       people: country.people.map(p => {
+        if (p.animals.length === 0) {
+          return ('No animals');
+        }
         const countAnimals = p.animals.length;
         return {
           name: `${p.name} [${countAnimals}]`,
